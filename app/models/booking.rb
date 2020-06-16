3c99438d643061532049a052cb860266cdda6099
Artem <skrartrom@icloud.com>
2020-06-16 14:12:15 +0300

    FIX: validation for correct date and time

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,18 +8,20 @@ class Booking < ApplicationRecord
   validate :correct_date_and_time
 
   def correct_date_and_time
-    date_start = Date.new(2020, 6, 13)
-    date_finish = Date.new(2021, 6, 13)
-    date_for_cleaning = date
-    if (date_start <= date_for_cleaning) && (date_for_cleaning <= date_finish)
-      time_start = Time.new(2020, 6, 13, 9).to_s(:time)
-      time_finish = Time.new(2021, 6, 13, 18).to_s(:time)
-      cleaning_time = time.to_s(:time)
-      unless (time_start <= cleaning_time) && (cleaning_time <= time_finish)
-        errors.add(:time, "should be in 09.00..18.00")
+    date_start = Date.today
+    date_finish = Date.new(2021)
+    unless (date_for_cleaning = date).nil?
+      if (date_start <= date_for_cleaning) && (date_for_cleaning <= date_finish)
+        time_start = Time.new(2020, 6, 13, 9).to_s(:time)
+        time_finish = Time.new(2021, 6, 13, 18).to_s(:time)
+        unless (cleaning_time = time).nil?
+          unless (time_start <= cleaning_time.to_s(:time)) && (cleaning_time.to_s(:time) <= time_finish)
+            errors.add(:time, "should be in 09.00..18.00")
+          end
+        end
+      else
+        errors.add(:date, "should be the correct.")
       end
-    else
-      errors.add(:date, "should be the correct.")
     end
   end
 end
